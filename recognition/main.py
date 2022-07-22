f86d8df95c9c89600546a2a9b137981db99a2dc5
fahla <nunofahla@ua.pt>
2022-07-22 02:10:36 +0100

    flask fix

--- recognition/main.py
+++ recognition/main.py
@@ -8,33 +8,31 @@ from io import BytesIO
 
 app = Flask(__name__)
 
+# allocate resources
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# list of possible flowers
 labels= pd.read_json("./labels.json", typ='series')
 all_labels=[]
 for i in range(len(labels)):
     all_labels.append(labels[i+1])
 
-test_transforms = transforms.Compose([transforms.Resize(255),
-                                      transforms.CenterCrop(224),
-                                      transforms.ToTensor(),
-                                      transforms.Normalize([0.485, 0.456, 0.406],
-                                                           [0.229, 0.224, 0.225])])
 model = torch.load("./densenet_model.pth")
 
 
 def predict(model, image):
     image = parseImage(image)
     results = {}
-    optimizer.zero_grad()
     image = image.to(device)
     outputs = model(image)
     _, preds = torch.max(outputs.data, 1)
     for o in range(len(outputs[0])):
         results[o] = float(outputs[0][o])
-
+    print("results: ", results)
     return results
 
 def parseImage(img):
+    # formats image to be compatible with the image samples
     image = Image.open(img)
 
     transform = transforms.Compose([transforms.Resize(255),
@@ -49,9 +47,10 @@ def parseImage(img):
 
 @app.route("/predict")
 def classify():
-    # guardar imagem quem vem no request no disco
-    # e passar-lhe o path em Image.open() depois
-    img = Image.open(path)
+    path = request.args.get("url")
+    mid = urllib.request.urlretrieve(path, "image.png")
+    img = Image.open("image.png")
+    print(path)
     return predict(model, img)
 
 
